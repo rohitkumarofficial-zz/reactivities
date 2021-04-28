5e5f4763b66ea08b7f1549941981382866db1ff2
rohitkumarofficial <rohitkumarjajoriya@gmail.com>
2021-04-28 11:43:11 +0530

    Dbcontext file created and added in startup class

--- API/Startup.cs
+++ API/Startup.cs
@@ -11,17 +11,20 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
         {
-            Configuration = configuration;
+            _config = config;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,6 +34,10 @@ namespace API
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+
+            services.AddDbContext<DataContext>(opt => {
+                opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
         }
 
