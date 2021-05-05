dbd16ed39ca32aff567cc75cf9f05cdc47319058
rohitkumarofficial <rohitkumarjajoriya@gmail.com>
2021-05-05 10:38:56 +0530

    react-router-dom package added for routing

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,4 @@
 import { Grid } from 'semantic-ui-react'
-import { Activity } from '../../../models/activity';
 import ActivityList from './ActivityList';
 import ActivityDetails from './../details/ActivityDetails';
 import ActivityForm from './../form/ActivityForm';
