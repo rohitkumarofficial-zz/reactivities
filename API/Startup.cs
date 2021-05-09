d21ff0f0e3554d18e791519bcba7574308649fe2
rohitkumarofficial <rohitkumarjajoriya@gmail.com>
2021-05-09 10:53:57 +0530

    FluentValidator nuget package added for validation

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,6 @@
 using API.Extensions;
+using Application.Activities;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,7 +23,10 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers().AddFluentValidation(_config => {
+                _config.RegisterValidatorsFromAssemblyContaining<Create>();
+            });
+
             services.AddApplicationServices(_config);
         }
 
