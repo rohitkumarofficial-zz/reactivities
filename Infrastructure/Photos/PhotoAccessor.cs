7c3ae37882e17c92f1571e32d809e786cfbd1731
rohitkumarofficial <rohitkumarjajoriya@gmail.com>
2021-06-09 10:45:33 +0530

    added cloudinary package for image upload on cloud and added interface to upload and delete file

---
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Application.Photos;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Photos
+{
+    public class PhotoAccessor : IPhotoAcccessor
+    {
+        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeletePhoto(string publicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
