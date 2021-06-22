1e90ecbde2f4661d0bcbc9ea30c1098c1657ed7b
rohitkumarofficial <rohitkumarjajoriya@gmail.com>
2021-06-22 11:12:10 +0530

    image upload at backend side using cloudinary

---
+++ Application/Photos/Delete.cs
@@ -0,0 +1,63 @@
+﻿using Application.Core;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Photos
+{
+    public class Delete
+    {
+        public class Command: IRequest<Result<Unit>>
+        {
+            public string Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<Unit>>
+        {
+            private readonly DataContext _dataContext;
+            private readonly IPhotoAcccessor _photoAcccessor;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext dataContext, IPhotoAcccessor photoAcccessor, IUserAccessor userAccessor)
+            {
+                _dataContext = dataContext;
+                _photoAcccessor = photoAcccessor;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var user = await _dataContext.Users.Include(p => p.Photos)
+                   .FirstOrDefaultAsync(x => x.UserName == _userAccessor.GetUsername());
+
+                if (user == null) return null;
+
+                var photo = user.Photos.FirstOrDefault(x => x.Id == request.Id);
+
+                if (photo == null) return null;
+
+                if (photo.IsMain) return Result<Unit>.Failure("you can not delete main photo");
+
+                var result = await _photoAcccessor.DeletePhoto(photo.Id);
+
+                if (result == null) return Result<Unit>.Failure("Problem while deleting photo from cloudinary");
+
+                user.Photos.Remove(photo);
+
+                var success = await _dataContext.SaveChangesAsync() > 0;
+
+                if (success) return Result<Unit>.Success(Unit.Value);
+
+                return Result<Unit>.Failure("problem deleting photo from API");
+
+            }
+        }
+    }
+}
