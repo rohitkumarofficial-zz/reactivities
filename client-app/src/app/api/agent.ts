99503beedd711f6c265c41f3e45013e7e9e0ca21
rohitkumarofficial <rohitkumarjajoriya@gmail.com>
2021-07-19 16:31:20 +0530

    photo upload, crop and preview done from react

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import { Activity, ActivityFormValues } from './../models/activity';
 import { history } from './../../index';
 import { store } from './../stores/store';
-import { Profile } from '../models/profile';
+import { Photo, Profile } from '../models/profile';
 
 const delay = (delay: number) => {
     return new Promise((resolve) => {
@@ -84,8 +84,19 @@ const Account = {
 }
 
 const Profiles = {
-    get: (username: string) => request.get<Profile>(`/profiles/${username}`)
+    get: (username: string) => request.get<Profile>(`/profiles/${username}`),
+    uploadPhoto: (file: Blob) => {
+
+        let formData = new FormData();
+        formData.append('File', file);
+        return axios.post<Photo>('photos', formData, {
+            headers: {'Content-type' : 'multipart/form-data'}
+        })
+    },
+    setMainPhoto: (id: string) => request.post(`/photos/${id}/setMain`, {}),
+    deletePhoto: (id: string) => request.delete(`/photos/${id}`)
 }
+
 const agent = {
     Activities,
     Account,
--- client-app/src/app/common/image-upload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/image-upload/PhotoUploadWidget.tsx
@@ -1,21 +1,58 @@
 import { observer } from 'mobx-react-lite'
-import React from 'react'
-import { Grid, Header } from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react'
+import { Button, Grid, Header, Image } from 'semantic-ui-react'
+import PhotoWidgetCropper from './PhotoWidgetCropper';
+import PhotoWidgetDropZone from './PhotoWidgetDropzone';
+import { Cropper } from 'react-cropper';
+
+interface Props {
+    uploadPhoto: (file: Blob) => void;
+    uploading: boolean;
+}
 
 export default observer(
-    function PhotoUploadWidget () {
+    function PhotoUploadWidget ({uploadPhoto, uploading} : Props) {
+        const [files, setFiles] = useState<any>([]);
+        const [cropper, setCropper] = useState<Cropper>();
+
+        function onCrop () {
+            if (cropper) {
+                cropper.getCroppedCanvas().toBlob(blob => {
+                    uploadPhoto(blob!);
+                })
+            }
+        }
+
+        useEffect(() => {
+            return () => {
+                files.forEach((file: any) => URL.revokeObjectURL(file.preview))
+            }
+        }, [files])
         return (
             <Grid>
                 <Grid.Column width={4}>
                     <Header sub color='teal' content='Step 1 - Add Photo' />
+                    <PhotoWidgetDropZone setFiles={setFiles} />
                 </Grid.Column>
                 <Grid.Column width={1} />
                 <Grid.Column width={4}>
                     <Header sub color='teal' content='Step 2 - Resize Image' />
+                    {files?.length > 0 && (
+                        <PhotoWidgetCropper setCropper={setCropper} imagePreview={files[0].preview} />
+                    )}
                 </Grid.Column>
                 <Grid.Column width={1} />
                 <Grid.Column width={4}>
                     <Header sub color='teal' content='Step 3 - Preview & Upload' />
+                    {files?.length > 0 &&
+                        <>
+                            <div className='image-preview' style={{ minHeight: 200, overflow: 'hidden' }} />
+                            <Button.Group>
+                                <Button onClick={onCrop} positive icon='check' loading={uploading} disabled={uploading}/>
+                                <Button onClick={() => setFiles([])} icon='close' disabled={uploading}/>
+                            </Button.Group>
+                        </>
+                    }
                 </Grid.Column>
             </Grid>
         )
